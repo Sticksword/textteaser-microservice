bde8dea11352e430da9fb31a9e8713776532881a
Michael <3814531+Sticksword@users.noreply.github.com>
2020-03-02 23:27:49 -0800

    add summarize endpoint with newspaper3k and nltk dependency, updated the deployment ymls

--- app/main.py
+++ app/main.py
@@ -1,9 +1,33 @@
-from flask import Flask
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from flask import Flask, request, jsonify
+from textteaser import TextTeaser
+from newspaper import Article
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/summarize')
 def hello():
-    return "Hello from Python!"
+  url = request.args.get('url', '')
+  print(url)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+  article = Article(url)
+  article.download()
+  article.parse()
+
+  title = article.title
+  print(title)
+  text = article.text
+  print(text)
+
+  tt = TextTeaser()
+
+  sentences = tt.summarize(title, text)
+
+  for sentence in sentences:
+    print(sentence)
+
+  return jsonify(sentences)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
--- app/textteaser/__init__.py
+++ app/textteaser/__init__.py
@@ -8,7 +8,7 @@ class TextTeaser(object):
     def __init__(self):
         self.summarizer = Summarizer()
 
-    def summarize(self, title, text, category="Undefined", source="Undefined", count=5):
+    def summarize(self, title, text, category="Undefined", source="Undefined", count=7):
         result = self.summarizer.summarize(text, title, source, category)
         result = self.summarizer.sortSentences(result[:count])
         # result = [res['sentence'] for res in result]
